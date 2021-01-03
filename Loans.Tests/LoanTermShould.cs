32a41f5a8aa9b0013f1adc5cabd154498fb158a7
marco villanueva <villanuevamarco98@gmail.com>
2021-01-02 20:29:11 -0700

    Nunit initial test

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Loans.Domain.Applications;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        public void ReturnTermInMonths()
+        {
+            //sut = system under test
+            //hiding default cnstrctr of 
+            //LoanTerm causes red squiggly
+            //see line 12 of LoanTerm.cs
+            var sut = new LoanTerm(2);
+        }
+    }
+}
