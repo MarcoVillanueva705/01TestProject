e2d8e3255a1a5aa1359d2e35b6fd422e7d5fd5ef
marco villanueva <villanuevamarco98@gmail.com>
2021-01-04 20:32:49 -0700

    contraint model of assertions

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 using Loans.Domain.Applications;
 
 namespace Loans.Tests
@@ -9,13 +9,18 @@ namespace Loans.Tests
     [TestFixture]
     public class LoanTermShould
     {
+        [Test]
         public void ReturnTermInMonths()
         {
             //sut = system under test
             //hiding default cnstrctr of 
             //LoanTerm causes red squiggly
             //see line 12 of LoanTerm.cs
-            var sut = new LoanTerm(2);
+            LoanTerm sut = new LoanTerm(2); //Arrange
+
+            //constraint model of assertions
+            //Assert.That(test result, constraint instance)
+            Assert.That(sut.ToMonths(), Is.EqualTo(24));  //Assert
         }
     }
 }
