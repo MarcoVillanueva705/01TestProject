8b3e80249aeaab25a07a6e498f2cd307a784116d
marco villanueva <villanuevamarco98@gmail.com>
2021-01-05 21:08:58 -0700

    The Logical Arrange, Act, Assert Test Phases

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -21,6 +21,18 @@ namespace Loans.Tests
             //constraint model of assertions
             //Assert.That(test result, constraint instance)
             Assert.That(sut.ToMonths(), Is.EqualTo(24));  //Assert
+
+            //Arrange: set up test object(s), initialize test data, etc
+
+            //var sut = new LoanTerm(1);
+
+            //Act: call method, set property, etc.
+
+            //var numberOfMonths = sut.ToMonths();
+
+            //Assert: compare returned value/end state with expected
+
+            //Assert.That(numberOfMonths, IsEqual.To(12));
         }
     }
 }
