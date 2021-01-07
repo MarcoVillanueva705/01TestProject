32f2705d8edf0f4ae5fb04c8e9a1e1b3bde5175d
marco villanueva <villanuevamarco98@gmail.com>
2021-01-06 20:54:19 -0700

    Years property test

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -22,9 +22,14 @@ namespace Loans.Tests
             //Assert.That(test result, constraint instance)
             Assert.That(sut.ToMonths(), Is.EqualTo(24));  //Assert
 
-            //Arrange: set up test object(s), initialize test data, etc
+            //Arrange: set up test object(s), initialize test data, thing
+            //we want to test
+        }
+        [Test]
+        public void storeYears()
+        {
 
-            //var sut = new LoanTerm(1);
+            var sut = new LoanTerm(1);
 
             //Act: call method, set property, etc.
 
@@ -32,7 +37,8 @@ namespace Loans.Tests
 
             //Assert: compare returned value/end state with expected
 
-            //Assert.That(numberOfMonths, IsEqual.To(12));
+            Assert.That(sut.Years, Is.EqualTo(12));
         }
+        
     }
 }
