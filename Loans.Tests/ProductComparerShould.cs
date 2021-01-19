0257654cad73e1d035b397a9d237fff7bf5668f3
marco villanueva <villanuevamarco98@gmail.com>
2021-01-18 20:45:15 -0700

    new test class

---
+++ Loans.Tests/ProductComparerShould.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    public class ProductComparerShould
+    {
+        [Test]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+
+        }
+    }
+}
