ef477ff897f798af783cbfb33e6e417a9a402c2c
marco villanueva <villanuevamarco98@gmail.com>
2021-01-19 20:59:10 -0700

    ReturnCorrectNumber test

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+//using directive for LoanProduct
+using Loans.Domain.Applications;
 
 namespace Loans.Tests
 {
@@ -10,7 +12,14 @@ namespace Loans.Tests
         [Test]
         public void ReturnCorrectNumberOfComparisons()
         {
+            var products = new List<LoanProduct>()
+            {
 
+            new LoanProduct(1, "a", 1)
+            };
+            //ProductComparer has a number of params to provide
+            //in the constructor; 200K decimal literal (hence the 'm')
+            var sut = new ProductComparer(new LoanAmount("USD" ,200_000m), products);
         }
     }
 }
