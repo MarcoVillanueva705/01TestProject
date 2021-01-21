852b7d6f1218e64e2e05f2d0e1bfc26760b60a7d
marco villanueva <villanuevamarco98@gmail.com>
2021-01-20 20:16:14 -0700

    testing products list

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -14,12 +14,21 @@ namespace Loans.Tests
         {
             var products = new List<LoanProduct>()
             {
-
+            
+                //we expect one product in the 
+                //comparison output
             new LoanProduct(1, "a", 1)
             };
             //ProductComparer has a number of params to provide
             //in the constructor; 200K decimal literal (hence the 'm')
-            var sut = new ProductComparer(new LoanAmount("USD" ,200_000m), products);
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+
+            List<MonthlyRepaymentComparison> compare = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //the Has helper class
+            //var sut has 'products' list passed as param
+            Assert.That(compare, Has.Exactly(1).Items);
         }
     }
 }
