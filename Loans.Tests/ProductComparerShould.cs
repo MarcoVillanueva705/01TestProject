2f2f2e5a1a2a7c00b48002d74d6302bc2168fecb
marco villanueva <villanuevamarco98@gmail.com>
2021-01-23 19:32:44 -0700

    no duplicates test passes

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -28,8 +28,34 @@ namespace Loans.Tests
 
             //the Has helper class
             //var sut has 'products' list passed as param
+            //**Below tests how many items in the list there are
             Assert.That(compare, Has.Exactly(1).Items);
             //was studying SQL
         }
+
+        [Test]
+        public void ReturnNotDuplicates()
+        {
+            var products = new List<LoanProduct>()
+            {
+            
+                //we expect one product in the 
+                //comparison output
+            new LoanProduct(1, "a", 1)
+            };
+            //ProductComparer has a number of params to provide
+            //in the constructor; 200K decimal literal (hence the 'm')
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+
+            List<MonthlyRepaymentComparison> compare = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //the Has helper class
+            //var sut has 'products' list passed as param
+            //**Below tests if list of items are unique**
+            Assert.That(compare, Is.Unique);
+            //was studying SQL
+        }
+
     }
 }
